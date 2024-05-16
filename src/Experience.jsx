8b1702addb651adf5ddb00cfa8a33a4b17af6b75
Bargid <120350488+Bargid@users.noreply.github.com>
2024-05-16 15:50:33 -0400

    changement du laptop

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -27,7 +27,7 @@ export default function Experience() {
         </mesh>
 
          {/* Render the WaterScene component */}
-         <WaterScene />
+         {/* <WaterScene /> */}
         
     </>
 }
